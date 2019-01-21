e7950bea8dac868edbf7e696224e9d5d131386da
Harmel Tristan <tristan.harmel@get.omp.eu>
2019-01-21 10:34:19 +0100

    fix up data path

--- rho_factor/config.py
+++ rho_factor/config.py
@@ -4,7 +4,14 @@ import os
 
 dir = '/DATA/git/rho_factor' #os.path.dirname(os.path.abspath(__file__))
 
-M2015_file = './data/aux/rhoTable_Mobley2015.csv'
-M1999_file = './data/aux/rhoTable_Mobley1999.csv'
-rhosoaa_fine_file = './data/aux/surface_reflectance_factor_rho_fine_aerosol_rg0.06_sig0.46.csv'
-rhosoaa_coarse_file = './data/aux/surface_reflectance_factor_rho_coarse_aerosol_rg0.60_sig0.60.csv'
+root = os.path.dirname(os.path.abspath(__file__))
+
+M2015_file = os.path.join(root, '../data/aux/rhoTable_Mobley2015.csv')
+M1999_file = os.path.join(root, '../data/aux/rhoTable_Mobley1999.csv')
+rhosoaa_fine_file = os.path.join(root, '../data/aux/surface_reflectance_factor_rho_fine_aerosol_rg0.06_sig0.46.csv')
+rhosoaa_coarse_file = os.path.join(root, '../data/aux/surface_reflectance_factor_rho_coarse_aerosol_rg0.60_sig0.60.csv')
+#
+# M2015_file = './data/aux/rhoTable_Mobley2015.csv'
+# M1999_file = './data/aux/rhoTable_Mobley1999.csv'
+# rhosoaa_fine_file = './data/aux/surface_reflectance_factor_rho_fine_aerosol_rg0.06_sig0.46.csv'
+# rhosoaa_coarse_file = './data/aux/surface_reflectance_factor_rho_coarse_aerosol_rg0.60_sig0.60.csv'
